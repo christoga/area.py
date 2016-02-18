002243934d676b831bfbc7e01a70c1e2c594f3b4
Andre Christoga <andrechristoga@gmail.com>
2016-02-18 21:02:24 +0700

    Triangle shape script

---
+++ area/triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def main():
+	'''
+	Area of the triangle
+	'''
+    pedestal = input("Insert the pedestal ")
+    height = input("Insert the height ")
+       
+    area = pedestal * height / 2
+    print "Area of the triangle is : ", area
+
+if __name__ == '__main__':
+	main()
