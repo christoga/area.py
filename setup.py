5914264721039996e19efa9e0290bdb0e7a6ccd9
Andre Christoga <andrechristoga@gmail.com>
2016-03-01 13:52:04 +0700

    Use function rather than new file

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 import os
 
-def main():		
+def main():
 	input = raw_input("What's the shape? ")
 
-	if input == 'triangle':
-		os.system("area/triangle.py")
+	if input == 'triangle':		
+		triangle()
+
+def triangle():
+	pedestal = input("Insert triangle pedestal ")
+	height = input("Insert triangle height ")
+
+	result = pedestal * height / 2
+	print "Result:", result
 
 if __name__ == '__main__':
 	main()
