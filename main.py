c32b16ed6789a10bac7a73b27f63b6c95a8e62e0
Andre Christoga <andrechristoga@gmail.com>
2016-03-01 14:32:07 +0700

    Circle shape function

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ import os
 def main():
 	input = raw_input("What's the shape? ")
 
-	if input == 'triangle':		
+	if input == 'triangle':
 		triangle()
+	if input == 'circle':
+		circle()
 
 def triangle():
 	pedestal = input("Insert triangle pedestal ")
 	height = input("Insert triangle height ")
 
 	result = pedestal * height / 2
+	print "Result:", result
+
+def circle():
+	radiant = input("Insert the radiant ")
+
+	result = radiant * 2 / 3.14
 	print "Result:", result
 
 if __name__ == '__main__':
